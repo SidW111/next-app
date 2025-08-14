e28210da6bffbfd5936e19367c637c3b03dce062
SidW111 <siddhantwainngade@gmail.com>
2025-08-14 14:57:54 +0530

    prisma generated successfully

--- app/api/v1/signup/route.ts
+++ app/api/v1/signup/route.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
-// import { PrismaClient } from "@/app/generated/prisma";
-//  const prismaClient = new PrismaClient();
+import { PrismaClient } from "@/app/generated/prisma";
+const prismaClient = new PrismaClient();
 export async function POST(req: NextRequest) {
   const data = await req.json();
-//   await prismaClient.user.create({
-//     data: { username: data.username, password: data.password },
-//   });
+  await prismaClient.user.create({
+    data: { username: data.username, password: data.password },
+  });
   console.log(data);
   return NextResponse.json({
     message: "User sign up successful",
